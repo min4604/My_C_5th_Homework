05a18254929e0835f16170c53a0e70ab60595a5a
min4604 <mina4604sh@gmail.com>
2021-11-28 20:58:11 +0800

    p13ok

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # p5  finish
 # p6  finish
 # p10 finish
+# p13 finish
---
+++ p13/source/main.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void cubeByference(int &nPtr);
+
+int main(void)
+{
+	int number = 5;
+	printf("The original value of number is %d", number);
+
+	cubeByference(number);
+
+	printf("\nThe new value of number is %d\n", number);
+
+	system("pause");
+	return 0;
+}
+
+void cubeByference(int &nPtr)
+{
+	nPtr = nPtr * nPtr * nPtr;
+}
